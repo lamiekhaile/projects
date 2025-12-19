2b9e26f4187b86d10bf56b9688dfa1ea26888f57
Lamiek Haile <hailelamiek41@gmail.com>
2025-12-18 21:49:55 -0500

    Add Vigenere cipher implementation

---
+++ Vigenère.py
@@ -0,0 +1,36 @@
+
+
+
+
+def vigenere(message, code):
+    
+    
+    note = list(message.upper())
+    hidden = list(code.upper())
+
+    while len(hidden) < len(note):
+        hidden.append(hidden[len(hidden) % len(code)])
+
+    final = []
+    for i in range(len(note)):
+        v = (ord(note[i])-ord('A') + ord(hidden[i])-ord('A')) % 26
+        num = chr(v + ord('A'))
+        final.append(num) 
+        
+
+
+    
+
+    
+
+
+    return ''.join(final)
+
+
+print(vigenere('message', 'code'))
+
+
+
+
+
+
